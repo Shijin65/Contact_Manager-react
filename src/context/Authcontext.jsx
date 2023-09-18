6a5caf59ba06aa6528aee9ffe1443a188f3b63b9
shijin65 <shijincht65@gmail.com>
2023-09-18 06:45:36 +0530

    installed axios

--- src/context/Authcontext.jsx
+++ src/context/Authcontext.jsx
@@ -1,5 +1,5 @@
 import {Children, createContext, useState} from 'react';
-
+import axios from 'axios';
 
 const Authcontext = createContext()
 export const Authcontextprovider = ({children})=>{
@@ -9,13 +9,18 @@ export const Authcontextprovider = ({children})=>{
 const loginUser = async()=>{
 
     try {
-        const res = await fetch(`http://localhost:5000/api/user/login`,
-        {   method:"POST",
-            headers:{"content-Type":"application/json"},
-            body : JSON.stringify({...userData})
-    })
-        const user=await res.json();
-        console.log(user)
+
+        axios.get(`http://localhost:5000/api/user/login`).then((respoonce)={
+            console
+        })
+
+    //     const res = await fetch(`http://localhost:5000/api/user/login`,
+    //     {   method:"POST",
+    //         headers:{"content-Type":"application/json"},
+    //         body : JSON.stringify({...userData})
+    // })
+    //     const user=await res.json();
+    //     console.log(user)
     } catch (error) {
         console.log( error)
     }
